dcb12cb0dac674014daf5bdfcff313001eca8bb7
Kaoutar <bendidikaoutar05@gmail.com>
2026-02-18 20:58:09 +0000

    add polygon calculator project

--- rgp_character/create_character.py
+++ rgp_character/create_character.py
@@ -1,33 +1,7 @@
-** start of main.py **
+import pdb
 
-full_dot = '●'
-empty_dot = '○'
+def divide(a, b):
+    pdb.set_trace()
+    return a / b
 
-def  create_character(name, strength, intelligence, charisma):
-    if not isinstance(name, str):
-        return 'The character name should be a string'
-    if name=='':
-        return 'The character should have a name'
-    if len(name)>10:
-        return'The character name is too long'
-    if ' ' in name:
-        return 'The character name should not contain spaces'
-    if not isinstance(strength, int) or not isinstance(intelligence, int) or not isinstance(charisma, int) :
-        return 'All stats should be integers'
-    if strength<1 or intelligence<1 or charisma<1:
-       return 'All stats should be no less than 1'
-    if strength>4 or intelligence>4 or charisma>4:
-        return 'All stats should be no more than 4'
-    if intelligence+charisma+strength!=7:
-        return 'The character should start with 7 points'
-    result = name + '\n'
-    result += 'STR ' + full_dot * strength + empty_dot * (10 - strength) + '\n'
-    result += 'INT ' + full_dot * intelligence + empty_dot * (10 - intelligence) + '\n'
-    result += 'CHA ' + full_dot * charisma + empty_dot * (10 - charisma)
-    return result
-
-create_character('ren',4,2,1)
-
-
-** end of main.py **
-
+print(divide(10, 2))
